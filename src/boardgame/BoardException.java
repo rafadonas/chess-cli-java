58fbe06c66cb4b5cafcf65e7e03454829e97f625
rafadonas <rafadonas.dev@gmail.com>
2025-08-30 17:56:42 -0300

    feat: add classes ChessException and ChessPosition

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,10 +1,7 @@
 package boardgame;
 
 public class BoardException extends RuntimeException{
-    public static final Long serialVersionUID = 1L;
-
-    public BoardException(String msg) {
-        super(msg);
+    public BoardException(String message) {
+        super(message);
     }
-
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,9 +22,13 @@ public class ChessMatch {
         }
         return mat;
     }
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
 
     public void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1) );
-        board.placePiece(new King(board, Color.BLACK), new Position(5, 3) );
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 4, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 2, new King(board, Color.BLACK));
     }
 }
