7c399653c3ff4de12b61aeec18310c17fa720901
rafadonas <rafadonas.dev@gmail.com>
2025-09-11 12:26:52 -0300

    fix: better treatment to specialmove promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,7 +43,12 @@ public class Program {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.println("Enter piece for promotion (B/N/R/Q): ");
-                    String type = scanner.nextLine();
+                    String type = scanner.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = scanner.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             }
